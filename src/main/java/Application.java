5f3106948a506a89165470cded3c30ebf58f3404
Anton <belix888@yandex.ru>
2023-04-28 21:44:44 +0500

    Решение второй части задания.

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,16 +1,27 @@
-import java.sql.*;
-import java.util.List;
-
 public class Application {
     public static void main(String[] args) {
-        BookDAO bookDAO = new BookDAOImpl();
-        List<Book> books = bookDAO.getAllBooks();
+        EmployeeDAO employeeDAO = new EmployeeDAOImpl();
+        System.out.println(employeeDAO.getAllEmployee());
+        Employee employee = new Employee(1, "Alexander", "Mirov", "man", 59, new City(1,"Moscow"));
+        employeeDAO.add(employee);
+        System.out.println(employeeDAO.getAllEmployee());
+        System.out.println();
+        employee.setFirst_name("Kirill");
+        employeeDAO.updateEmployee(4, employee);
+        System.out.println(employeeDAO.getById(4));
+        employeeDAO.deleteEmployee(5);
+        System.out.println();
+        System.out.println(employeeDAO.getAllEmployee());
 
-        for (Book book : books) {
-            System.out.println("Book ID: " + book.getId());
-            System.out.println("Title: " + book.getTitle());
-            System.out.println("Author_id: " + book.getAuthorId());
-            System.out.println("Amount: " + book.getAmount());
-        }
+//        List<Employee> employees = employeeDAO.getOneEmployee();
+//
+//        for (Employee employee : employees) {
+//            System.out.println("id: " + employee.getId());
+//            System.out.println("first_name: " + employee.getFirst_name());
+//            System.out.println("last_name: " + employee.getLast_name());
+//            System.out.println("gender: " + employee.getGender());
+//            System.out.println("age: " + employee.getAge());
+//            System.out.println("city_id: " + employee.getCity());
+//        }
     }
 }
---
+++ src/main/java/City.java
@@ -0,0 +1,11 @@
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class City {
+    private int city_id;
+    private String city_name;
+}
